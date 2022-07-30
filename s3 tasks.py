7c0c8e7b8e6ff26dc323db80cd3db9b8719b4239
unknown <Dadoush@outlook.de>
2022-07-30 22:58:23 +0200

    add first solve to s3 tasks.py

---
+++ s3 tasks.py
@@ -0,0 +1,28 @@
+print("===================================================================")
+print("Hi, my name is Tarek, and we're gonna solve the tasks together :)")
+print("===================================================================")
+
+input('Press any key to continue...')
+
+print("What should I call you?")
+username = input()
+
+print(f"Welcome {username}, let's do the tasks together :D")
+
+print(f"so {username}, our first task is: how to create a bootlean.")
+
+print("Let me please ask you one more time about your name ;)")
+while True:
+    
+    if username == input():
+        print("Ok that's it")
+        break
+    else:
+        print("False, enter it one more time")
+        
+
+
+input('Press any key to continue...')
+    
+
+
