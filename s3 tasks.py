19f0eea823e7380eb33bd0e181bdf47d3445ba3e
unknown <Dadoush@outlook.de>
2022-07-31 03:58:29 +0200

    solve more tasks in s3 tasks.py

--- s3 tasks.py
+++ s3 tasks.py
@@ -20,9 +20,94 @@ while True:
     else:
         print("False, enter it one more time")
         
+input('Press any key to continue...')
 
+from time import sleep
+
+y = 5
+print('In our seconde task we have to make an integer variable and name it "y"')
+print('y = 5') ; sleep(1)
+print(y) ; sleep(1)
+print(type(y))
 
 input('Press any key to continue...')
-    
+
+z = 2.5
+print('The third task is to create a float variable like "z = 2.5"') ; sleep(1)
+print(z) ; sleep(1)
+print(type(z))
+
+input('Press any key to continue...')
+
+print('4- String variable is just like my name "Tarek"')
+s = 'Tarek'
+print('s = Tarek') ; sleep(1)
+print(s) ; sleep(1)
+print(type(s))
+
+input('Press any key to continue...')
+
+print('5- Do you remember that "y"? It is an integer variable and we have to convert it into a float variable.') 
+print('y = float(y)') ; sleep(1)
+y = float(y)
+print(y) ; sleep(1)
+print(type(y))
+
+input('Press any key to continue...')
+
+print("6- No, we can't")
+
+input('Press any key to continue...')
+
+print("7- The answer is..") ; sleep(1)
+print("l = [1,2,3,4,5]")
+l = [1,2,3,4,5]
+print(l) ; sleep(1)
+print(type(l))
+
+input('Press any key to continue...')
+
+print("8- tuple is being created..") ; sleep(1)
+print("t = (10,11,12,13,14,15)")
+t = (10,11,12,13,14,15)
+print(t) ; sleep(1)
+print(type(t))
+
+input('Press any key to continue...')
+
+print("9- Converting..") ; sleep(1)
+print("l = tuple(l)")
+l = tuple(l)
+print(l) ; sleep(1)
+print(type(l))
+
+input('Press any key to continue...')
+
+print("10- Creating..") ; sleep(1)
+print("d = {'Tarek':1000, 'Mystro':2000, 'Mahmoud': 3000}")
+d = {'Tarek':1000, 'Mystro':2000, 'Mahmoud': 3000}
+print(d) ; sleep(1)
+print(type(d))
+
+input('Press any key to continue...')
+
+print("11- Yes, we can but it is not necessary")
+
+input('Press any key to continue...')
+
+print("12- Python is interpreted")
+
+input('Press any key to continue...')
+
+
+
+
+
+
+
+
+
+
+
 
 
