6ee3d93aab046d72089acb6b44e88641a7b45a9f
unknown <Dadoush@outlook.de>
2022-08-01 16:16:23 +0200

    add tasks operators file

---
+++ Modules tasks.py
@@ -0,0 +1,61 @@
+print('1- = is an assignment operator, == means "equal to"')
+
+input("Press any key to continue...")
+
+print('2- != means "not equal to"')
+
+input("Press any key to continue...")
+
+print("3- it's like the symbols in maths, so each symbol has its priority")
+
+input("Press any key to continue...")
+
+print("4- x = 10")
+x = 10
+print(x)
+
+input("Press any key to continue...")
+
+print("5- x = 15 + x")
+x = 15 + x
+print(x)
+
+input("Press any key to continue...")
+
+print("6- x = x/5")
+x = x/5
+print(x)
+
+input("Press any key to continue...")
+
+print("7- x = x*10")
+x = x*10
+print(x)
+
+input("Press any key to continue...")
+
+print("8- x%3")
+print(x%3)
+
+input("Press any key to continue...")
+
+print("9- 11%4")
+print(11%4)
+
+input("Press any key to continue...")
+
+print("10- 2**3")
+print(2**3)
+
+input("Press any key to continue...")
+
+print("11- 11/3")
+print(11/3)
+
+input("Press any key to continue...")
+
+print("No, we can't as you can see below ↓↓")
+print(type(11/3))
+
+
+
