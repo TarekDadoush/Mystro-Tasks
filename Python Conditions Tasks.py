c6c74ce0cf24134ac056b908b2f0cb3f5d6464f4
unknown <Dadoush@outlook.de>
2022-08-03 13:43:51 +0200

    add Python Conditions Tasks

---
+++ Python Conditions Tasks.py
@@ -0,0 +1,104 @@
+print("1- Checking whether 10 is bigger than 15...")
+print("10 > 15")
+print(10 > 15)
+
+input("Press any key to continue...")
+
+print("2- Printing...")
+x = 10
+
+if 15 > x:
+    print("x smaller than 15")
+
+
+input("Press any key to continue...")
+
+print('3- When we have so many entries, so instead of writing "data one and data two and data three", we have only to put comma after each entry instead of AND')
+
+input("Press any key to continue...")
+
+print(" 4- And stands for listing, or stands to choose one of different choices")
+
+input("Press any key to continue...")
+
+print("5- Everything in ALL should be True, in Any only one from two or more conditions should be True")
+
+input("Press any key to continue...")
+
+print("6- We will use ALL")
+
+input("Press any key to continue...")
+
+print("7- IF is to start the conditions, elif to add more conditions (e.g checking password whether correct or not then check if the password correct after that) before we type else")
+
+input("Press any key to continue...")
+
+print("8- elif to remain the condition and else is to end it")
+
+input("Press any key to continue...")
+
+print("9- Yes")
+
+input("Press any key to continue...")
+
+print("10- No")
+
+input("Press any key to continue...")
+
+print("11- Building the ternary operator...")
+
+username = 'Tarek'
+password = 12345
+
+username = 'Tarek'
+password = 12345
+
+print("Welcome", username,"!") if username == 'Tarek' and password == 12345 else print("Username or password incorrect, please try again or register")
+
+input("Press any key to continue...")
+
+print("12- Whenever the condition in IF is correct, Python will ignore everything else")
+
+input("Press any key to continue...")
+
+print("13- Just to end the part of the condition")
+
+input("Press any key to continue...")
+
+print("14- 1. Checking...")
+l = [2,4,6,8,10]
+
+if 4 in l:
+    print (True)
+else:
+    print(False)
+
+input("Press any key to continue...")
+
+print("14- 2. Checking...")
+
+if not 4 and 6 in l:
+    print (True)
+else:
+    print(False)
+
+input("Press any key to continue...")
+
+print("14- 3. Checking...")
+
+if 3 or 6 in l:
+    print(True)
+else:
+    print(False)
+
+input("Press any key to continue...")
+
+if all([2,4,5 in l]):
+    print(True)
+else:
+    print(False)
+
+
+
+
+
